a26dabd0a329883b2417dfea362b8b7a7230b0b0
Ellen <elianora123@yahoo.com>
2021-06-06 21:45:10 +0400

    I have done 2 homeworks

---
+++ script.js
@@ -0,0 +1,11 @@
+// let currentYear = 2021;
+// let birthYear  = 1991;
+// let age  = currentYear - birthYear;
+// console.log('I am ' + age + ' or ' + (age - 1));
+
+let celsius = 30;
+let celsiusInF = (celsius*9)/5 + 32;
+console.log(celsius + '°C is ' + celsiusInF + '°F');
+let fahrenheit = 20;
+let fahrenheitInC = ((fahrenheit - 32)*5)/9;
+console.log(fahrenheit + '°F is ' + fahrenheitInC + '°C');
